6a1ef3083fadc6cd8bedcd972688a8ccccaa5131
Anjal Afghan <anjalafghan@Anjals-MacBook-Pro.local>
2020-03-25 19:08:35 +0530

    Alpha ver 1. Added card layout and created a database. Imported the data to the card

--- dataDisplay.php
+++ dataDisplay.php
@@ -1,7 +1,11 @@
+<?php
+$connection = new mysqli(localhost, anjal, root, aayur);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
+
      <!--Import Google Icon Font-->
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
      <!--Import materialize.css-->
@@ -14,7 +18,34 @@
     <title>Aayur all in one Shoppy</title>
 </head>
 <body>
-    Click here to see all the products: <a href="dataDisplay.php">Click Here</a>
+<?php 
+$data = $connection->query("SELECT * FROM PRODUCTS;");
+while($row = $data->fetch_assoc()){
+$product = $row['Products'];
+$price   = $row['Price'];
+
+?>
+<div class="container">
+<div class="row">
+    <div class="col s12 m7 l4 offset-m1">
+      <div class="card">
+        <div style="font-color: black" class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title"><?php echo "$product"; ?></span>
+        </div>
+        <div class="card-content">
+          <p><?php echo "$price"; ?></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<?php
+}
+
+?>
     <script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 </html>
